0ecf79528d38ce9b37e2c4b27b935f59fb19cab0
1cetsw <1cetsw@gmail.com>
2022-09-20 21:11:49 +0200

    Initial commit

---
+++ data_manager.py
@@ -0,0 +1,330 @@
+from datetime import datetime
+import connection
+
+
+@connection.connection_handler
+def get_all_answers(cursor):
+    cursor.execute("""SELECT * FROM answer;""")
+    answers = cursor.fetchall()
+    return answers
+
+
+@connection.connection_handler
+def get_all_questions(cursor):
+    cursor.execute("""SELECT * FROM question;""")
+    questions = cursor.fetchall()
+    return questions
+
+
+@connection.connection_handler
+def get_all_comments(cursor):
+    cursor.execute("""SELECT * FROM comment;""")
+    comments = cursor.fetchall()
+    return comments
+
+
+@connection.connection_handler
+def get_all_question_headers(cursor):
+    cursor.execute("""SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
+                      WHERE TABLE_NAME = 'question';""")
+    table_headers = cursor.fetchall()
+    return table_headers
+
+
+@connection.connection_handler
+def get_all_answer_headers(cursor):
+    cursor.execute("""SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
+                      WHERE TABLE_NAME = 'answer';""")
+    table_headers = cursor.fetchall()
+    return table_headers
+
+
+@connection.connection_handler
+def write_to_questions(cursor, data):
+    cursor.execute("""INSERT INTO question VALUES (%(id_value)s, %(submission_time_value)s, %(view_number_value)s, 
+                    %(vote_number_value)s, %(title_value)s, %(message_value)s, %(image_value)s);""",
+                   {'id_value': data['id'],
+                    'submission_time_value': data['submission_time'],
+                    'view_number_value': data['view_number'],
+                    'vote_number_value': data['vote_number'],
+                    'title_value': data['title'],
+                    'message_value': data['message'],
+                    'image_value': data['image']})
+
+
+@connection.connection_handler
+def write_to_answers(cursor, data):
+    cursor.execute("""INSERT INTO answer VALUES (%(id_value)s, %(submission_time_value)s, %(vote_number_value)s, 
+                    %(question_id_value)s, %(message_value)s, %(image_value)s);""",
+                   {'id_value': data['id'],
+                    'submission_time_value': data['submission_time'],
+                    'vote_number_value': data['vote_number'],
+                    'question_id_value': data['question_id'],
+                    'message_value': data['message'],
+                    'image_value': data['image']})
+
+
+@connection.connection_handler
+def write_to_comments(cursor, data):
+    cursor.execute("""INSERT INTO comment VALUES (%(id_value)s, %(question_id_value)s, %(answer_id_value)s, 
+                    %(message_value)s, %(submission_time_value)s, %(edited_count_value)s);""",
+                   {'id_value': get_next_comment_id(),
+                    'question_id_value': data['question_id'] if data['type'] == 'question' else None,
+                    'answer_id_value': data['answer_id'] if data['type'] == 'answer' else None,
+                    'message_value': data['message'],
+                    'submission_time_value': datetime.now().replace(microsecond=0),
+                    'edited_count_value': None})
+
+
+@connection.connection_handler
+def sort_questions_by_time(cursor):
+    cursor.execute("""SELECT title, id FROM question
+                      ORDER BY submission_time DESC;""")
+    sorted_questions = cursor.fetchall()
+    return sorted_questions
+
+
+@connection.connection_handler
+def get_next_question_id(cursor):
+    try:
+        cursor.execute("""SELECT MAX(id) from question;""")
+        new_id = cursor.fetchall()[0]['max'] + 1
+    except KeyError:
+        new_id = 0
+    return new_id
+
+
+@connection.connection_handler
+def get_next_answer_id(cursor):
+    try:
+        cursor.execute("""SELECT MAX(id) from answer;""")
+        new_id = cursor.fetchall()[0]['max'] + 1
+    except KeyError:
+        new_id = 0
+    return new_id
+
+
+@connection.connection_handler
+def get_next_comment_id(cursor):
+    try:
+        cursor.execute("""SELECT MAX(id) from comment;""")
+        new_id = cursor.fetchall()[0]['max'] + 1
+    except KeyError:
+        new_id = 0
+    return new_id
+
+
+@connection.connection_handler
+def get_question_by_id(cursor, question_id):
+    cursor.execute("""SELECT * FROM question
+                      WHERE id=%(id)s;""",
+                   {'id': question_id})
+    question_data = cursor.fetchall()[0]
+    return question_data
+
+
+@connection.connection_handler
+def get_answer_by_id(cursor, answer_id):
+    cursor.execute("""SELECT * FROM answer
+                      WHERE id=%(id)s;""",
+                   {'id': answer_id})
+    answer_data = cursor.fetchall()[0]
+    return answer_data
+
+
+@connection.connection_handler
+def get_comment_by_id(cursor, comment_id):
+    cursor.execute("""SELECT * FROM comment
+                      WHERE id=%(id)s;""",
+                   {'id': comment_id})
+    comment_data = cursor.fetchall()[0]
+    return comment_data
+
+
+@connection.connection_handler
+def get_answers_by_question_id(cursor, question_id):
+    cursor.execute("""SELECT * FROM answer
+                      WHERE question_id=%(id)s;""",
+                   {'id': question_id})
+    answers = cursor.fetchall()
+    return answers
+
+
+@connection.connection_handler
+def get_comments_by_question_id(cursor, question_id):
+    cursor.execute("""SELECT * FROM comment
+                      WHERE question_id=%(id)s;""",
+                   {'id': question_id})
+    comments = cursor.fetchall()
+    return comments
+
+
+@connection.connection_handler
+def get_answer_comments_by_question_id(cursor, question_id):
+    cursor.execute("""SELECT * FROM comment
+                      WHERE question_id=%(id)s;""",
+                   {'id': question_id})
+    comments = cursor.fetchall()
+    return comments
+
+
+@connection.connection_handler
+def edit_question(cursor, question_id, edited_data):
+    cursor.execute("""UPDATE question
+                      SET submission_time = %(submission_time_value)s, title = %(title_value)s, 
+                      message = %(message_value)s, image = %(image_value)s
+                      WHERE id=%(id)s;""",
+                   {'submission_time_value': datetime.now().replace(microsecond=0),
+                    'title_value': edited_data['title'],
+                    'message_value': edited_data['message'],
+                    'image_value': edited_data['image'],
+                    'id': question_id})
+
+
+@connection.connection_handler
+def edit_answer(cursor, answer_id, edited_data):
+    cursor.execute("""UPDATE answer
+                      SET submission_time = %(submission_time_value)s, message = %(message_value)s,
+                      image = %(image_value)s
+                      WHERE id=%(id)s;""",
+                   {'submission_time_value': datetime.now().replace(microsecond=0),
+                    'message_value': edited_data['message'],
+                    'image_value': edited_data['image'],
+                    'id': answer_id})
+
+
+@connection.connection_handler
+def edit_comment(cursor, comment_id, edited_data):
+    cursor.execute("""UPDATE comment
+                      SET submission_time = %(submission_time_value)s, message = %(message_value)s,
+                      edited_count = %(edited_count_value)s
+                      WHERE id=%(id)s;""",
+                   {'message_value': edited_data['message'],
+                    'submission_time_value': datetime.now().replace(microsecond=0),
+                    'edited_count_value': edited_data['edited_count'],
+                    'id': comment_id})
+
+
+@connection.connection_handler
+def delete_question_by_id(cursor, question_id):
+    cursor.execute("""DELETE FROM comment
+                      WHERE question_id=%(id)s;""",
+                   {'id': question_id})
+    cursor.execute("""SELECT id FROM answer
+                      WHERE question_id=%(id)s;""",
+                   {'id': question_id})
+    answer_ids = cursor.fetchall()
+    for answer_id in answer_ids:
+        delete_answer_by_id(answer_id['id'])
+    cursor.execute("""DELETE FROM question
+                      WHERE id=%(id)s;""",
+                   {'id': question_id})
+
+
+@connection.connection_handler
+def delete_answer_by_id(cursor, answer_id):
+    cursor.execute("""DELETE FROM comment
+                      WHERE answer_id=%(id)s;""",
+                   {'id': answer_id})
+    cursor.execute("""DELETE FROM answer
+                      WHERE id=%(id)s;""",
+                   {'id': answer_id})
+
+
+@connection.connection_handler
+def delete_comment_by_id(cursor, comment_id):
+    cursor.execute("""DELETE FROM comment
+                      WHERE id=%(id)s;""",
+                   {'id': comment_id})
+
+
+def add_new_question():
+    new_question_data = {
+        'id': get_next_question_id(),
+        'submission_time': datetime.now().replace(microsecond=0),
+        'view_number': 0,
+        'vote_number': 0
+        }
+    return new_question_data
+
+
+def add_new_answer(new_answer, question_id):
+    new_data = {
+        "id": get_next_answer_id(),
+        "submission_time": datetime.now().replace(microsecond=0),
+        "vote_number": "0",
+        "question_id": question_id,
+        "message": new_answer,
+        "image": ""
+    }
+    write_to_answers(new_data)
+
+
+@connection.connection_handler
+def get_all_answers_by_id_ordered_by_vote_number(cursor, question_id):
+    cursor.execute("""SELECT * FROM answer
+                      WHERE question_id=%(id)s
+                      ORDER BY vote_number DESC;""",
+                   {'id': question_id})
+    answers = cursor.fetchall()
+    return answers
+
+
+@connection.connection_handler
+def vote_up_for_questions(cursor, question_id):
+    cursor.execute("""UPDATE question
+                          SET vote_number = vote_number + 1
+                          WHERE id=%(id)s;""",
+                   {'id': int(question_id)})
+
+
+@connection.connection_handler
+def vote_down_for_questions(cursor, question_id):
+    cursor.execute("""UPDATE question
+                          SET vote_number = vote_number - 1
+                          WHERE id=%(id)s;""",
+                   {'id': int(question_id)})
+
+
+@connection.connection_handler
+def vote_up_for_answers(cursor, answer_id):
+    cursor.execute("""UPDATE answer
+                          SET vote_number = vote_number + 1
+                          WHERE id = %(id)s;""",
+                   {'id': answer_id})
+
+
+@connection.connection_handler
+def vote_down_for_answers(cursor, answer_id):
+    cursor.execute("""UPDATE answer
+                          SET vote_number = vote_number - 1
+                          WHERE id = %(id)s;""",
+                   {'id': answer_id})
+
+
+@connection.connection_handler
+def search_in_questions(cursor, search_phrase):
+    cursor.execute("""SELECT * FROM question
+                      WHERE title LIKE %(search_phrase)s OR message LIKE %(search_phrase)s;""",
+                   {'search_phrase': '%' + search_phrase + '%'})
+    question_data = cursor.fetchall()
+    return question_data
+
+
+@connection.connection_handler
+def search_in_answers(cursor, search_phrase):
+    cursor.execute("""SELECT * FROM answer
+                      WHERE message LIKE %(search_phrase)s;""",
+                   {'search_phrase': '%' + search_phrase + '%'})
+    answer_data = cursor.fetchall()
+    return answer_data
+
+
+@connection.connection_handler
+def get_latest_questions(cursor, count):
+    cursor.execute("""SELECT * FROM question
+                      ORDER BY submission_time DESC
+                      LIMIT %(count)s;""",
+                   {'count': count})
+    latest_questions = cursor.fetchall()
+    return latest_questions
---
+++ server.py
@@ -0,0 +1,193 @@
+from flask import Flask, render_template, redirect, request, url_for
+import data_manager
+
+app = Flask(__name__)
+
+latest_opened_question_id = 0
+
+
+@app.route('/')
+def index():
+    latest_questions = data_manager.get_latest_questions(5)
+    return render_template('index.html', questions=latest_questions)
+
+
+@app.route('/list')
+def route_to_all_questions():
+    sorted_questions = data_manager.sort_questions_by_time()
+    return render_template('all_questions.html', questions=sorted_questions)
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    search_phrase = request.form.get('search_phrase')
+    questions = data_manager.search_in_questions(search_phrase)
+    answers = data_manager.search_in_answers(search_phrase)
+    return render_template('search.html', search_phrase=search_phrase, questions=questions, answers=answers)
+
+
+@app.route('/question/<question_id>')
+def display_question(question_id):
+    global latest_opened_question_id
+    latest_opened_question_id = question_id
+    question = data_manager.get_question_by_id(question_id)
+    answers = data_manager.get_all_answers_by_id_ordered_by_vote_number(question_id)
+    comments = data_manager.get_all_comments()
+    return render_template('display_question.html', question=question, answers=answers, comments=comments)
+
+
+
+@app.route('/questions/<question_id>/vote-up', methods=['POST'])
+def question_vote_up(question_id):
+    data_manager.vote_up_for_questions(question_id)
+    return redirect(url_for('display_question', question_id=question_id))
+
+
+@app.route('/questions/<question_id>/vote-down', methods=['POST'])
+def question_vote_down(question_id):
+    data_manager.vote_down_for_questions(question_id)
+    return redirect(url_for('display_question', question_id=question_id))
+
+
+@app.route('/answers/<question_id>/<answer_id>/vote-up', methods=['POST'])
+def answer_vote_up(question_id, answer_id):
+    data_manager.vote_up_for_answers(answer_id)
+    return redirect(url_for('display_question', question_id=question_id))
+
+
+@app.route('/questions/<question_id>/<answer_id>/vote-down', methods=['POST'])
+def answer_vote_down(question_id, answer_id):
+    data_manager.vote_down_for_answers(answer_id)
+    return redirect(url_for('display_question', question_id=question_id))
+
+
+@app.route('/add-question', methods=['GET', 'POST'])
+def add_question():
+    if request.method == "GET":
+        return render_template('new_question.html')
+
+    new_question_all_data = data_manager.add_new_question()
+    new_question_all_data.update(
+        {
+            'title': request.form.get('question'),
+            'message': request.form.get('message'),
+            'image': request.form.get('image')
+        }
+    )
+    data_manager.write_to_questions(new_question_all_data)
+    return redirect(url_for('display_question', question_id=new_question_all_data['id']))
+
+
+@app.route('/question/<question_id>/edit', methods=['GET', 'POST'])
+def edit_question(question_id):
+    if request.method == "GET":
+        question = data_manager.get_question_by_id(question_id)
+        return render_template('edit_question.html', question=question)
+
+    edited_question_data = {
+            'title': request.form.get('question'),
+            'message': request.form.get('message'),
+            'image': request.form.get('image')
+            }
+    data_manager.edit_question(question_id, edited_question_data)
+    return redirect(url_for('display_question', question_id=question_id))
+
+
+@app.route('/question/<question_id>/new-comment', methods=['GET', 'POST'])
+def add_comment_to_question(question_id):
+    if request.method == "GET":
+        question = data_manager.get_question_by_id(question_id)
+        return render_template('add_comment_to_question.html', question=question)
+
+    new_comment_to_question = {
+        'message': request.form.get('comment'),
+        'type': 'question',
+        'question_id': question_id
+        }
+    data_manager.write_to_comments(new_comment_to_question)
+    global latest_opened_question_id
+    return redirect(url_for('display_question', question_id=latest_opened_question_id))
+
+
+@app.route('/question/<question_id>/delete')
+def delete_question(question_id):
+    data_manager.delete_question_by_id(question_id)
+    return redirect(url_for('route_to_all_questions'))
+
+
+@app.route("/question/<question_id>/new-answer", methods=['GET', 'POST'])
+def add_new_answer(question_id):
+    if request.method == "GET":
+        return render_template("new_answer.html", question_id=question_id)
+
+    new_answer = request.form["new_answer"]
+    data_manager.add_new_answer(new_answer, question_id)
+    return redirect(url_for("display_question", question_id=question_id))
+
+
+@app.route('/answer/<answer_id>/edit', methods=['GET', 'POST'])
+def edit_answer(answer_id):
+    if request.method == "GET":
+        answer = data_manager.get_answer_by_id(answer_id)
+        return render_template('edit_answer.html', answer=answer)
+
+    question_id = data_manager.get_answer_by_id(answer_id)['question_id']
+    edited_answer_data = {
+            'message': request.form.get('message'),
+            'image': request.form.get('image')
+            }
+    data_manager.edit_answer(answer_id, edited_answer_data)
+    return redirect(url_for('display_question', question_id=question_id))
+
+
+@app.route('/answer/<answer_id>/<question_id>/new-comment', methods=['GET', 'POST'])
+def add_comment_to_answer(answer_id, question_id):
+    if request.method == "GET":
+        answer = data_manager.get_answer_by_id(answer_id)
+        question = data_manager.get_question_by_id(question_id)
+        return render_template('add_comment_to_answer.html', answer=answer, question=question)
+
+    new_comment_to_answer = {
+        'message': request.form.get('comment'),
+        'type': 'answer',
+        'answer_id': answer_id
+        }
+    data_manager.write_to_comments(new_comment_to_answer)
+    global latest_opened_question_id
+    return redirect(url_for('display_question', question_id=latest_opened_question_id))
+
+
+@app.route('/answer/<answer_id>/delete')
+def delete_answer(answer_id):
+    data_manager.delete_answer_by_id(answer_id)
+    global latest_opened_question_id
+    return redirect(url_for('display_question', question_id=latest_opened_question_id))
+
+
+@app.route('/comment/<comment_id>/edit', methods=['GET', 'POST'])
+def edit_comment(comment_id):
+    if request.method == "GET":
+        comment = data_manager.get_comment_by_id(comment_id)
+        return render_template('edit_comment.html', comment=comment)
+
+    comment = data_manager.get_comment_by_id(comment_id)
+    if comment['question_id']:
+        question_id = comment['question_id']
+    else:
+        question_id = data_manager.get_answer_by_id(comment['answer_id'])['question_id']
+    edited_comment_data = {
+        'message': request.form.get('message'),
+        'edited_count': comment['edited_count'] + 1 if type(comment['edited_count']) is int else 1}
+    data_manager.edit_comment(comment_id, edited_comment_data)
+    return redirect(url_for('display_question', question_id=question_id))
+
+
+@app.route('/comment/<comment_id>/delete')
+def delete_comment(comment_id):
+    data_manager.delete_comment_by_id(comment_id)
+    global latest_opened_question_id
+    return redirect(url_for('display_question', question_id=latest_opened_question_id))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
